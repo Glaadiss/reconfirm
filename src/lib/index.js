6f7f39b5bfd185e284ae5c35e9e9971642030c21
Glaadiss <bartekgladys@gmail.com>
2018-05-21 12:43:58 +0200

    Change import path for withConfirm

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,5 @@
 import DialogView from "./elements/DialogView";
-import withConfirm from "./elements/DialogView";
+import withConfirm from "./elements/withConfirm";
 import { confirm } from "./elements/confirm";
 import { confirmReducer } from "./elements/confirmReducer";
 
